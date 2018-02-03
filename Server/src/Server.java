e8d5cc83baac9d7a58d4a0729698f7625a85b91d
Kofi <kufuorkofi@gmail.com>
2018-02-03 16:10:15 +0000

    Add podThread functionality which identifies incoming data by its CMD code, saves it and prints it

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Server for Mission Control
@@ -69,9 +74,177 @@ public class Server {
     private static class PodThread extends Thread {
 
         Socket podSocket;
-
+        private Scanner is = null;
+        private PrintWriter os = null;
+        String data = "";
+        
         public PodThread(Socket podSocket) {
             this.podSocket = podSocket;
+        }
+        
+        public void run(){
+        
+            try {
+                //opens input and output streams on the podsocket
+                is = new Scanner(podSocket.getInputStream());
+                os = new PrintWriter(podSocket.getOutputStream());
+                
+                int acceleration = 0, temperature = 0, stripe_count = 0, 
+                        distance = 0, position = 0, velocity = 0, 
+                        ground_proximity =0, battery_voltage = 0, 
+                        battery_current = 0, battery_temperature = 0, 
+                        pod_temperature = 0, rail_proximity=0, pump1 =0,
+                        pump2 =0, accumulator1 =0, accumulator2 = 0; 
+                
+                while(true){
+                
+                    //whilst there is a connection any data that is sent is identified by its CMD code, saved and outputted 
+                    if (!is.hasNext()) return;
+                    data = is.nextLine();
+                    switch(data.substring(0, 5))
+                        {
+                              case "CMD01":
+                                sendConfirmationToPod(podSocket);
+                                acceleration = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("acceleration: "+acceleration);
+                                break;
+                              case "CMD02":
+                                sendConfirmationToPod(podSocket);
+                                velocity = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("velocity: "+velocity);
+                                break;
+                              case "CMD03":
+                                sendConfirmationToPod(podSocket);
+                                position = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("position: "+position);
+                                break;
+                              case "CMD04":
+                                sendConfirmationToPod(podSocket);
+                                temperature = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("temperature: "+temperature);
+                                break;
+                              case "CMD05":
+                                sendConfirmationToPod(podSocket);
+                                stripe_count = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("stripe count: "+stripe_count);
+                                break;
+                              case "CMD06":
+                                sendConfirmationToPod(podSocket);
+                                distance = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("distance: "+distance);
+                                break;
+                              case "CMD07":
+                                sendConfirmationToPod(podSocket);
+                                ground_proximity = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("ground proximity: "+ground_proximity);
+                                break;
+                              case "CMD08":
+                                sendConfirmationToPod(podSocket);
+                                rail_proximity = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("rail proxmity: "+rail_proximity);
+                                break;
+                              case "CMD09":
+                                sendConfirmationToPod(podSocket);
+                                battery_temperature = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("battery temperature: "+battery_temperature);
+                                break;
+                              case "CMD10":
+                                sendConfirmationToPod(podSocket);
+                                battery_current = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("current: "+battery_current);
+                                break;
+                              case "CMD11":
+                                sendConfirmationToPod(podSocket);
+                                battery_voltage = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("voltage: "+battery_voltage);
+                                break;
+                              case "CMD12":
+                                sendConfirmationToPod(podSocket);
+                                accumulator1 = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("accumulator1: "+accumulator1);
+                                break;
+                              case "CMD13":
+                                sendConfirmationToPod(podSocket);
+                                accumulator2 = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("accumulator2: "+accumulator2);
+                                break;
+                              case "CMD14":
+                                sendConfirmationToPod(podSocket);
+                                pump1 = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("pump1: "+pump1);
+                                break;
+                              case "CMD15":
+                                sendConfirmationToPod(podSocket);
+                                pump2 = 
+                                        (int) Math.round(Double.parseDouble
+                                        (
+                                                data.substring(5))
+                                        );
+                                System.out.println("pump2: "+pump2);
+                                break;
+                                                           
+                        }
+                }
+                
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
