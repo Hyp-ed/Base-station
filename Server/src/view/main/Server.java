54e50c2439df3f200116e6214782eb25637c0873
Kofi <kufuorkofi@gmail.com>
2018-03-31 11:50:39 +0100

    Fix error where button press was needed to receive further data

--- Server/src/view/main/Server.java
+++ Server/src/view/main/Server.java
@@ -58,41 +58,49 @@ public class Server extends Thread {
 
                     distance = (int) Math.round(Double.parseDouble(data.substring(5)));
                     System.out.println("distance: " + distance);
+                    sendHandshakeToPod();
                     break;
                 case "CMD02":
 
                     velocity = (int) Math.round(Double.parseDouble(data.substring(5)));
                     System.out.println("velocity: " + velocity);
+                    sendHandshakeToPod();
                     break;
                 case "CMD03":
 
                     acceleration = (int) Math.round(Double.parseDouble(data.substring(5)));
                     System.out.println("acceleration: " + acceleration);
+                    sendHandshakeToPod();
                     break;
                 case "CMD04":
 
                     stripe_count = (int) Math.round(Double.parseDouble(data.substring(5)));
                     System.out.println("stripe count: " + stripe_count);
+                    sendHandshakeToPod();
                     break;
                 case "CMD05":
 
                     rpm_fl = (int) Math.round(Double.parseDouble(data.substring(5)));
                     System.out.println("rpm fl: " + rpm_fl);
+                    sendHandshakeToPod();
                     break;
                 case "CMD06":
 
                     rpm_fr = (int) Math.round(Double.parseDouble(data.substring(5)));
                     System.out.println("rpm fr: " + rpm_fr);
+                    sendHandshakeToPod();
                     break;
                 case "CMD07":
 
                     rpm_bl = (int) Math.round(Double.parseDouble(data.substring(5)));
                     System.out.println("rpm bl: " + rpm_bl);
+                    sendHandshakeToPod();
                     break;
                 case "CMD08":
 
                     rpm_br = (int) Math.round(Double.parseDouble(data.substring(5)));
                     System.out.println("rpm br: " + rpm_br);
+                    sendHandshakeToPod();
                     break;
             }
                 //sendToSpaceX();
@@ -113,6 +121,22 @@ public class Server extends Thread {
             e.printStackTrace();
         }
         printWriter.println(message);
+        printWriter.flush();
+    }
+
+    public void sendHandshakeToPod() {
+        if (podSocket == null) {
+            System.out.println("ERROR: no pod found");
+            return;
+        }
+
+        PrintWriter printWriter = null;
+        try {
+            printWriter = new PrintWriter(podSocket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        printWriter.println(1);
         printWriter.flush();
     }
 
