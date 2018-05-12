2e7ae93ffbda73c05c9b1d9f61c466c90d932bad
Jam516 <kufuorkofi@gmail.com>
2018-05-12 20:56:02 +0100

    Update GUI with gauges

--- Server/src/view/main/Main.java
+++ Server/src/view/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main2.fxml"));
         primaryStage.setTitle("HYPED Mission Control System");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
