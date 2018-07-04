50b17cdb03c0d82268b1401985fbb164eeadb2ce
Isabella Chan <isabellachan426@gmail.com>
2018-07-04 22:57:33 +0100

    Kill java app on GUI window closed

--- Server/src/view/main/Main.java
+++ Server/src/view/main/Main.java
@@ -1,10 +1,12 @@
 package view.main;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
@@ -14,6 +16,12 @@ public class Main extends Application {
         primaryStage.setTitle("HYPED Mission Control System");
         primaryStage.setScene(new Scene(root, 1555, 801));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                System.out.println("Stage is closing");
+                System.exit(0);  // to ensure java app is killed
+            }
+        });
     }
 
     public static void main(String[] args) {
