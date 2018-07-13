38a2040792c9a9b9f060b45a4be7a685392bbf55
Jam516 <kufuorkofi@gmail.com>
2018-07-13 09:31:00 -0700

    Include mac gui

--- Server/src/view/main/Main.java
+++ Server/src/view/main/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main3.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main4.fxml"));
         primaryStage.setTitle("HYPED Mission Control System");
         primaryStage.setScene(new Scene(root, 1555, 801));
         primaryStage.show();
